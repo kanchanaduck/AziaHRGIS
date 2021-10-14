abef2b9c0d4c653b4761c477ceb89f74ab4f6b62
014496 <014496@CPT-2019-P01259>
2021-10-14 16:59:30 +0700

    collapse

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ChartsModule } from 'ng2-charts';
@@ -16,6 +17,7 @@ import { ChartsModule } from 'ng2-charts';
   declarations: [
     AppComponent,
     HeaderComponent,
+    SidebarComponent,
     FooterComponent,
     DashboardComponent,
   ],
--- src/app/shared/sidebar/sidebar.component.spec.ts
+++ src/app/shared/sidebar/sidebar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WelfareComponent } from './welfare.component';
+import { SidebarComponent } from './sidebar.component';
 
-describe('WelfareComponent', () => {
-  let component: WelfareComponent;
-  let fixture: ComponentFixture<WelfareComponent>;
+describe('SidebarComponent', () => {
+  let component: SidebarComponent;
+  let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WelfareComponent ]
+      declarations: [ SidebarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WelfareComponent);
+    fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
